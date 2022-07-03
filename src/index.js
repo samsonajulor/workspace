12a096fce940bc099f1a2c451ff9a8e606fca049
Samson Ajulor <samson@esoftresponse.com>
2022-07-03 11:48:49 +0100

    completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// Internet Explorer 11 requires polyfills and partially supported by this project.
+// import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/stable';
+import './i18n';
+import './styles/app-base.css';
+import './styles/app-components.css';
+import './styles/app-utilities.css';
+import { createRoot } from 'react-dom/client';
+import App from './app/App';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
