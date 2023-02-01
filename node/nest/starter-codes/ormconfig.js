1f802d8167cd05f7c4a2b5a2caff924ad33d2370
Samson Ajulor <samsonajulor@gmail.com>
2023-02-01 23:22:49 +0100

    updated the orm config

--- node/nest/starter-codes/ormconfig.js
+++ node/nest/starter-codes/ormconfig.js
@@ -23,6 +23,15 @@ switch (process.env.NODE_ENV) {
     });
     break;
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      database: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
     break;
   default:
     throw new Error('unknown environment');
