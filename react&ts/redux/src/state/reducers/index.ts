92009145301866ba4ef08cf55485a4327d35b6dd
Samson Ajulor <samsonajulor@gmail.com>
2023-02-10 07:41:13 +0100

    added redux ts

---
+++ react&ts/redux/src/state/reducers/index.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import repositoriesReducer from './repositoriesReducer';
+
+const reducers = combineReducers({
+  repositories: repositoriesReducer,
+});
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
