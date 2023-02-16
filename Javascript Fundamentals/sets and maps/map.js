8d1238c2ff9f0adda6e45c7e5432baffee3d435d
Samson Ajulor <samsonajulor@gmail.com>
2023-02-16 18:36:20 +0100

    added map

--- Javascript Fundamentals/sets and maps/map.js
+++ Javascript Fundamentals/sets and maps/map.js
@@ -1,10 +1,57 @@
-// what is a map in js
+// const map = new Map();
+// const set = new Set();
+
+// console.log(typeof map);
+// console.log(typeof set);
+
+/** Definition */
+// what is a map
 // a map is a collection of keyed data items
-// the difference between a map and an object is that a map can use any type of key
+// a map is different from an object because it allows us to use any type of key
 
-// const obj = {
-//   1: 'John',
-//   2: 30
-// }
+const data ={
+  "name": "map",
+  "age": 30,
+  1: "hello"
+  // "{"name": "map", "age": 30}": "hello"
+}
 
-// console.log(Object.keys(obj))
+console.log(Object.keys(data))
+//name is the key
+//map is the value
+
+// "long string".join()
+
+// data.name
+
+// why do we need map
+// getting the keys from an object always returns a string.
+
+//how to declare a map
+// we can declare a map using the map constructor
+// we can also declare a map using the map literal syntax
+
+// using a constructor example
+const map1 = new Map(
+ [
+  ['name', 'john'],
+  ['age', 30],
+  [true, 'hello'],
+  [1, 'how are you'],
+  [function(){}, 'goodbye']
+ ]
+);
+
+console.log(map1);
+
+// using a map literal syntax
+const map2 = new Map();
+
+map2.set('name', 'john');
+map2.set('age', 30);
+map2.set(true, 'hello');
+map2.set(function toString(){}, 'goodbye');
+
+map2.values
+map2.forEach
+
--- test.js
+++ test.js
@@ -1,32 +1,10 @@
-// what is a map in js
-// a map is a collection of keyed data items
-// it is a data structure that stores unique keys and their values
-// it is a collection of values that cannot contain duplicates
-
-// how to create a map
-// we can create a map using the map constructor
-// we can also create a map using the map literal syntax
-
-// using a constructor example
-const map1 = new Map();
-
-// using a map literal syntax
-const map2 = new Map([['a string', 'hello'], [1, 'how are you'], [true, 'goodbye']]);
-console.log(map2);
-
-// adding values to a map
-// we can add values to a map using the set method
-// we can also add values to a map using the map literal syntax
-
-// using the set method
-map1.set('a string', 'hello');
-map1.set(1, 'how are you');
-
-// using the map literal syntax
-const map2 = new Map([['a string', 'hello'], [1, 'how are you'], [true, 'goodbye']]);
-console.log(map2);
-
-// maps can use any type of key
-// we can use any type of key in a map
-// we can use strings, numbers, booleans, objects, arrays, functions, etc.
-
+// get()	Gets the value for a key in a Map
+//e.g. map1.get('name')
+// clear()	Removes all the elements from a Map
+// size	Returns the number of elements in a Map
+// delete()	Removes a Map element specified by a key
+// has()	Returns true if a key exists in a Map
+// forEach()	Invokes a callback for each key/value pair in a Map
+// entries()	Returns an iterator object with the [key, value] pairs in a Map
+// keys()	Returns an iterator object with the keys in a Map
+// values()	Returns an iterator object of the values in a Map
