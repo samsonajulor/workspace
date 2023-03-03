10cce0a4853a34faf000eb77b98fd01c84ca7aae
Samson Ajulor <samsonajulor@gmail.com>
2023-03-03 12:11:53 +0100

    added weighted uniform strings

--- algorithms/weightedUniformStrings.js
+++ algorithms/weightedUniformStrings.js
@@ -20,22 +20,49 @@
  * 4. create an array of the weights of the uniform subsequence of the given string
  * 5. create an array of 'Yes' or 'No' depending on whether the queries array contains the weights of the uniform subsequence of the given string
  */
-function weightedUniformStrings(str, queries) {
-  // 1. create a new string from the given string by removing all the duplicate characters
-  const newStr = str.split('').filter((char, index, arr) => char !== arr[index - 1]).join('');
-  // 2. create an array of the weights of the characters in the new string
-  const newStrWeights = newStr.split('').map(char => char.charCodeAt(0) - 96);
-  // 3. create an array of the weights of the characters in the given string
-  const strWeights = str.split('').map(char => char.charCodeAt(0) - 96);
-  // 4. create an array of the weights of the uniform subsequence of the given string
-  const uniformSubsequenceWeights = strWeights.reduce((acc, weight, index, arr) => {
-    if (weight === arr[index - 1]) {
-      acc.push(weight + arr[index - 1]);
+
+function weightedUniformStrings(s, queries) {
+  let weights = new Set();
+  let prevChar = '';
+  let currWeight = 0;
+  for (let i = 0; i < s.length; i++) {
+    let char = s.charAt(i);
+    if (char !== prevChar) {
+      currWeight = char.charCodeAt(0) - 96;
+    } else {
+      currWeight += char.charCodeAt(0) - 96;
     }
-    return acc;
-  }, []);
-  // 5. create an array of 'Yes' or 'No' depending on whether the queries array contains the weights of the uniform subsequence of the given string
-  return queries.map(query => newStrWeights.includes(query) || uniformSubsequenceWeights.includes(query) ? 'Yes' : 'No');
+    weights.add(currWeight);
+    prevChar = char;
+  }
+  let result = [];
+  for (let j = 0; j < queries.length; j++) {
+    if (weights.has(queries[j])) {
+      result.push('Yes');
+    } else {
+      result.push('No');
+    }
+  }
+  return result;
 }
+
+
+// function weightedUniformStrings(str, queries) {
+//   // 1. create a new string from the given string by removing all the duplicate characters
+//   const newStr = str.split('').filter((char, index, arr) => char !== arr[index - 1]).join('');
+//   // 2. create an array of the weights of the characters in the new string
+//   const newStrWeights = newStr.split('').map(char => char.charCodeAt(0) - 96);
+//   // 3. create an array of the weights of the characters in the given string
+//   const strWeights = str.split('').map(char => char.charCodeAt(0) - 96);
+//   // 4. create an array of the weights of the uniform subsequence of the given string
+//   const uniformSubsequenceWeights = strWeights.reduce((acc, weight, index, arr) => {
+//     if (weight === arr[index - 1]) {
+//       acc.push(weight + arr[index - 1]);
+//     }
+//     return acc;
+//   }, []);
+//   // 5. create an array of 'Yes' or 'No' depending on whether the queries array contains the weights of the uniform subsequence of the given string
+//   return queries.map(query => newStrWeights.includes(query) || uniformSubsequenceWeights.includes(query) ? 'Yes' : 'No');
+// }
 
 console.log(weightedUniformStrings('abccddde', [1, 3, 12, 5, 9, 10]));
